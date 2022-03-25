3f5bd8d16768729d8a45a6b9acd21450a94ee8ad
Nawtic <Nawtic@outlook.com>
2022-03-24 23:09:12 -0500

    Header and Footer compatibility fix.

---
+++ About/index.php
@@ -0,0 +1,15 @@
+<?php
+    include "../assets/Header.php";
+?>
+
+<html>
+    <body>
+        <div id="Page_Content">
+            <p>This page would contain information about the Kennel, but nothing has been added yet.</p>
+        </div>
+    </body>
+</html>
+
+<?php
+    include "../assets/Footer.html";
+?>
---
+++ Contact/index.php
@@ -0,0 +1,15 @@
+<?php
+    include "../assets/Header.php";
+?>
+
+<html>
+    <body>
+        <div id="Page_Content">
+            <p>This page would contain contact information for the Kennel.</p>
+        </div>
+    </body>
+</html>
+
+<?php
+    include "../assets/Footer.html";
+?>
