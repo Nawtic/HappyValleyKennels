b9200a5a1e6c3e350cfcd2758f2955c7be84b373
Nawtic@outlook.com <R7D3dCnDF`MxpFHku`rQ~VAde%PyDCCxfvPD#g`o%jD^otoRZSj&sDzHFr9~E%AVbAUAcwhA>
2022-04-04 10:10:14 -0600

    Add header and footer to scheduling sub-pages.

--- Scheduling/Check Out/checkout.php
+++ Scheduling/Check Out/checkout.php
@@ -1,35 +1,34 @@
+<?php
+include("../../assets/Header/Header.php");
+?>
+
+<!-- Page Credit: Andres Bastidas -->
+
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Check-Out</title>
-    <link href="checkout_style.css" rel="stylesheet" />
-    <link href="style_header_check.css" rel="stylesheet" />
 
-    <script>
-      function confirmation(){
-        window.location.href = "Confirmation";
-      }
-    </script>
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Check-Out</title>
+  <link href="checkout_style.css" rel="stylesheet" />
+  <link href="style_header_check.css" rel="stylesheet" />
 
-  </head>
-  <body>
-    <div id="Page_Header">
-      <a href="../Home">Happy Valley Kennels</a>
-    </div>
+  <script>
+    function confirmation() {
+      window.location.href = "Confirmation";
+    }
+  </script>
 
-    <ul id="Nav_Bar">
-      <li><a class="active" href="../Home">Home</a></li>
-      <li><a href="../About">About</a></li>
+</head>
 
-      <li><a href="../FAQ">FAQ</a></li>
-      <li><a href="../Contact">Contact</a></li>
-    </ul>
-    <div class="container">  
-      <center>  <h1> Check-Out </h1> </center>  
-      <hr>  
+<body>
+  <div class="container">
+    <center>
+      <h1> Check-Out </h1>
+    </center>
+    <hr>
     <form method="post">
       <div class="row">
         <div class="col-75">
@@ -38,61 +37,23 @@
               <div class="row">
                 <div class="col-50">
                   <h3>Billing Address</h3>
-                  <label for="fname"
-                    ><i class="fa fa-user"></i> Full Name</label
-                  >
-                  <input
-                    type="text"
-                    id="fname"
-                    name="firstname"
-                    placeholder="John M. Doe"
-                  />
-                  <label for="email"
-                    ><i class="fa fa-envelope"></i> Email</label
-                  >
-                  <input
-                    type="text"
-                    id="email"
-                    name="email"
-                    placeholder="john@example.com"
-                  />
-                  <label for="adr"
-                    ><i class="fa fa-address-card-o"></i> Address</label
-                  >
-                  <input
-                    type="text"
-                    id="adr"
-                    name="address"
-                    placeholder="542 W. 15th Street"
-                  />
-                  <label for="city"
-                    ><i class="fa fa-institution"></i> City</label
-                  >
-                  <input
-                    type="text"
-                    id="city"
-                    name="city"
-                    placeholder="New York"
-                  />
+                  <label for="fname"><i class="fa fa-user"></i> Full Name</label>
+                  <input type="text" id="fname" name="firstname" placeholder="John M. Doe" />
+                  <label for="email"><i class="fa fa-envelope"></i> Email</label>
+                  <input type="text" id="email" name="email" placeholder="john@example.com" />
+                  <label for="adr"><i class="fa fa-address-card-o"></i> Address</label>
+                  <input type="text" id="adr" name="address" placeholder="542 W. 15th Street" />
+                  <label for="city"><i class="fa fa-institution"></i> City</label>
+                  <input type="text" id="city" name="city" placeholder="New York" />
 
                   <div class="row">
                     <div class="col-50">
                       <label for="state">State</label>
-                      <input
-                        type="text"
-                        id="state"
-                        name="state"
-                        placeholder="NY"
-                      />
+                      <input type="text" id="state" name="state" placeholder="NY" />
                     </div>
                     <div class="col-50">
                       <label for="zip">Zip</label>
-                      <input
-                        type="text"
-                        id="zip"
-                        name="zip"
-                        placeholder="10001"
-                      />
+                      <input type="text" id="zip" name="zip" placeholder="10001" />
                     </div>
                   </div>
                 </div>
@@ -107,45 +68,20 @@
                     <i class="fa fa-cc-discover" style="color: orange"></i>
                   </div>
                   <label for="cname">Name on Card</label>
-                  <input
-                    type="text"
-                    id="cname"
-                    name="cardname"
-                    placeholder="Name on the card"
-                  />
+                  <input type="text" id="cname" name="cardname" placeholder="Name on the card" />
                   <label for="ccnum">Credit card number</label>
-                  <input
-                    type="text"
-                    id="ccnum"
-                    name="cardnumber"
-                    placeholder="1111-2222-3333-4444"
-                  />
+                  <input type="text" id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444" />
                   <label for="expmonth">Exp Month</label>
-                  <input
-                    type="text"
-                    id="expmonth"
-                    name="expmonth"
-                    placeholder="September"
-                  />
+                  <input type="text" id="expmonth" name="expmonth" placeholder="September" />
 
                   <div class="row">
                     <div class="col-50">
                       <label for="expyear">Exp Year</label>
-                      <input
-                        type="text"
-                        id="expyear"
-                        name="expyear"
-                        placeholder="2022"
-                      />
+                      <input type="text" id="expyear" name="expyear" placeholder="2022" />
                     </div>
                     <div class="col-50">
                       <label for="cvv">CVV</label>
-                      <input
-                        type="text"
-                        id="cvv"
-                        name="cvv"
-                        placeholder="352"
-                      />
+                      <input type="text" id="cvv" name="cvv" placeholder="352" />
                     </div>
                   </div>
                 </div>
@@ -157,5 +93,11 @@
         </div>
       </div>
     </form>
-  </body>
+  </div>
+</body>
+
 </html>
+
+<?php
+include("../../assets/Footer/Footer.php");
+?>
