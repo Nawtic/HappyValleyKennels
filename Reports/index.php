caaf5f5ca4444f97453d1a38d625c1b1c71a463d
Nawtic@outlook.com <R7D3dCnDF`MxpFHku`rQ~VAde%PyDCCxfvPD#g`o%jD^otoRZSj&sDzHFr9~E%AVbAUAcwhA>
2022-04-06 10:00:48 -0600

    index redirects to reports.php

--- Reports/index.php
+++ Reports/index.php
@@ -1,71 +1,10 @@
 <?php
-    include "../assets/Header/Header.php";
+    session_start();
 
     if(!isset($_SESSION["Role"]) AND $_SESSION["Role"] != "Employee"){
         header('Location: /HappyValleyKennels/Assets/Error/Access Denied');
     }
-?>
-
-<!-- Page Credit: Alexa Miller -->
-
-<html>
-    <head>
-        <title>Happy Valley Kennels</title>
-        <link rel="stylesheet" type="text/css" href="../reports.css">
-        <link rel="stylesheet" type="text/css" href="reports.css">
-    </head>
-    <body>
-        <div id="Page_Content">
-            <section>
-      <table>
-         <caption><h3>Reports</h3></caption>
-        <thead>
-            <tr>
-                <th class="left">Dog Name(s)</th>
-                <th class="left">Owner Name(s)</th>
-                <th class="left">Food Type</th>
-                <th class="left">Necessary Medical Info</th>
-                <th class="left">Paid?</th>
-                <th class="left">Date Start-End</th>
-                <th class="right">Comments</th>
-            </tr>
-        </thead>   
-        <tbody>
-            <tr class="blue">  
-                <td rowspan="1" class="middle"></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td rowspan="1" class="middle"></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td> 
-            </tr>
-            <tr class="blue">
-                <td rowspan="1" class="middle"></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td> 
-            </tr>
-        </tbody>
-    </table>
-</section>
-    
-        </div>
-    </body>
-</html>
-
-<?php
-    include "../assets/Footer/Footer.php";
+    else {
+        header('Location: /HappyValleyKennels/Reports/reports.php');
+    }
 ?>
