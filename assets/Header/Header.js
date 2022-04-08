a76264496e1792f2fbaebdeb1c0fed595107dcf5
Nawtic <Nawtic@outlook.com>
2022-04-07 23:02:14 -0500

    Header error handling

--- assets/Header/Header.js
+++ assets/Header/Header.js
@@ -3,26 +3,30 @@ function showDropdown() {
 }
 
 
-function hideDropdown(){
+function hideDropdown() {
     document.getElementById("dropdown_items").classList.remove("show");
 }
 
 function setActive() {
     //Get current url
     var url = window.location.href;
-    
+
     //Split url into list
     var splitUrl = url.split("/");
 
     //Get current directory name from url list
     var currentPage = splitUrl[4];
 
-    document.getElementById(currentPage).classList.toggle("active");
+    try {
+        document.getElementById(currentPage).classList.toggle("active");
+    } catch {}
 
-    var subpage = splitUrl[5]; 
+    var subpage = splitUrl[5];
 
     //Activate if a subpage is open
-    if((subpage != undefined) && (subpage != "")){
-        document.getElementById(subpage).classList.toggle("subActive");
+    if ((subpage != undefined) && (subpage != "")) {
+        try {
+            document.getElementById(subpage).classList.toggle("subActive");
+        } catch {}
     }
 }
