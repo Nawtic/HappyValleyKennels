3a7de3e002eeb97cb4aacdca9f17fa779a140a9e
Nawtic <Nawtic@outlook.com>
2022-04-13 10:42:42 -0600

    Messages can be saved in XML

--- Contact/send.php
+++ Contact/send.php
@@ -1,10 +1,59 @@
 <?php
+include("../assets/Header/Header.php");
+
+try {
     $dateAndTime = date("d-m-Y_H-i-s");
-    $message = fopen("Messages/".$dateAndTime.".txt", "w");
+    $filepath = "Messages/".$dateAndTime.".xml";
 
-    $messageContent = "Date: ".$dateAndTime."Name: ".$_POST["Name"]."\nPhone: ".$_POST["Phone"]."\nEmail: ".$_POST["Email"]."\nSubject: ".$_POST["Subject"]."\nMessage: ".$_POST["Message"];
+    $metaAttributes = array("date" => $dateAndTime, "subject" => $_POST["Subject"]);
+    $attributes = array("email" => $_POST["Email"], "phone" => $_POST["Phone"]);
 
-    fwrite($message, $messageContent);
+    $file = fopen($filepath, "w");
 
-    echo("Your message has been successfully sent.");
+    $writer = new XMLWriter();
+    $writer -> openMemory();
+    
+    $writer -> setIndent(1);
+    $writer -> setIndentString('    ');
+
+    $writer -> startDocument('1.0', 'utf-8');
+
+    $writer -> startElement("message");
+    foreach($attributes as $key => $val) {
+        $writer -> startAttribute($key);
+        $writer -> text($val);
+        $writer -> endAttribute();
+    }
+
+    $writer -> startElement("sender");
+
+    foreach($attributes as $key => $val) {
+        $writer -> startAttribute($key);
+        $writer -> text($val);
+        $writer -> endAttribute();
+    }
+
+    $writer -> text($_POST["Name"]);   
+    $writer -> endElement();
+
+    $writer -> startElement("content");
+    $writer -> text($_POST["Message"]);
+    $writer -> endElement();
+
+    $writer -> endElement();
+
+    
+    $writer->endDocument();
+
+    fwrite($file, $writer -> outputMemory());
+
+    echo("<div id=\"Page_Content\"><h1>Success</h1><p>Your message has been successfully sent. <br> We will get back to you as soon as we can.</p></div>");
+}
+
+catch (Exception $error){
+    echo("<h1>Error</h1><p>Your message could not be sent, please try again later.</p>");
+}
+
+include("../assets/Footer/Footer.php");
+
 ?>
