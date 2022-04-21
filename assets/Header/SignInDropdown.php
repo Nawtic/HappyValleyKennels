ea862b9f7b1fa26e06d561f6ecc9e0a79fd13959
Nawtic <Nawtic@outlook.com>
2022-04-21 17:30:13 -0500

    Sign In Changes

--- assets/Header/SignInDropdown.php
+++ assets/Header/SignInDropdown.php
@@ -1,14 +1,33 @@
 <div id="dropdown_items">
-    <form action="/HappyValleyKennels/Sign In/index.php" method="post">
-        <p>User ID</p>
-        <input class="field" type="text" name="ID"><br>
-
-        <p>Password</p>
-        <input class="field" type="password" name="Password"><br>
-        <div id="Submit_Container">
-            <input id="Submit_Button" type="submit" value="Sign In">
+    <div>
+        <div id="sign_in_options">
+            <button id="customer_sign_in_button" class="hide" onclick="toggleSignIn()">Customer Sign In</button>
+            <button id="employee_sign_in_button" onclick="toggleSignIn()">Employee Sign In</button>
         </div>
 
-        <p>Need an Account? <a href="/HappyValleyKennels/Sign Up">Sign Up</a></p>
-    </form>
+        <form id="customer_sign_in" action="/HappyValleyKennels/Sign In/index.php" method="post">
+            <p>Customer Email</p>
+            <input class="field" type="text" name="ID"><br>
+
+            <p>Password</p>
+            <input class="field" type="password" name="Password"><br>
+            <div id="Submit_Container">
+                <input id="Submit_Button" type="submit" value="Sign In">
+            </div>
+
+            <p>Need an Account?</p>
+            <a href="/HappyValleyKennels/Sign Up">Sign Up</a>
+        </form>
+
+        <form id="employee_sign_in" class="hide" action="/HappyValleyKennels/Sign In/index.php" method="post">
+            <p>Employee ID</p>
+            <input class="field" type="text" name="ID"><br>
+
+            <p>Password</p>
+            <input class="field" type="password" name="Password"><br>
+            <div id="Submit_Container">
+                <input id="Submit_Button" type="submit" value="Sign In">
+            </div>
+        </form>
+    </div>
 </div>
