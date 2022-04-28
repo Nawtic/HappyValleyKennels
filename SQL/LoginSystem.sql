2ffa98b8eb622ad4814e1818b9ec42513c253096
Nawtic <Nawtic@outlook.com>
2022-04-28 15:36:02 -0500

    Sign In tweaks

---
+++ SQL/LoginSystem.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS LoginSystem;
+CREATE DATABASE LoginSystem;
+USE LoginSystem;
+
+CREATE TABLE IF NOT EXISTS `users` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `username` varchar(50) NOT NULL,
+ `email` varchar(50) NOT NULL,
+ `password` varchar(50) NOT NULL,
+ `create_datetime` datetime NOT NULL,
+ PRIMARY KEY (`id`)
+);
+
+DROP USER IF EXISTS "customercheck"@"localhost";
+CREATE USER "customercheck"@"localhost" IDENTIFIED BY "loginmanage";
+GRANT SELECT, UPDATE ON LoginSystem.users TO "customercheck"@"localhost";
