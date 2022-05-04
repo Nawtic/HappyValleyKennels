21205d987461f6c852fd13d19c4b7f3ace2a6e8f
Nawtic <Nawtic@outlook.com>
2022-05-04 15:45:43 -0500

    Add table

--- SQL/registro_db.sql
+++ SQL/registro_db.sql
@@ -12,4 +12,18 @@ concern VARCHAR (300),
 received_date date
 );
 
-SELECT * FROM datos
+CREATE TABLE messages(
+	ID INT (3) PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100),
+    phone VARCHAR(11),
+    email VARCHAR(100),
+    subject VARCHAR(100),
+    message VARCHAR(300),
+	received_date date
+);
+
+INSERT INTO messages(name, phone, email, subject, message, received_date) VALUES ("Person", "11234567890", "person@email.com", "Dogs", "Message about dogs", now());
+
+SELECT * FROM datos;
+
+SELECT * FROM messages;
