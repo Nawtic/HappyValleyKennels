761c6d60d5ac29bab180e3d3e48ba15f8c7953af
Nawtic <Nawtic@outlook.com>
2022-05-06 03:08:43 -0500

    Updates to home

--- Home/index.php
+++ Home/index.php
@@ -1,7 +1,7 @@
 <?php
-    $page_creator = "Stephen Erichsen";
+$page_creator = "Stephen Erichsen";
 
-    include "../assets/Header/Header.php";
+include "../assets/Header/Header.php";
 ?>
 
 <!-- Page Credit: Stephen Erichsen-->
@@ -9,37 +9,40 @@
 <!DOCTYPE html>
 
 <html>
-    <head>
-        <link rel="stylesheet" type="text/css" href="Home.css">
-    </head>
 
-    <body>
-        
-        <div id="Page_Content">
-            <div id="row-1">
-                <div>
+<head>
+    <link rel="stylesheet" type="text/css" href="Home.css">
+</head>
+
+<body>
+
+    <div id="Page_Content">
+        <div id="row-1">
+            <div id="Intro_Text_Box">
+                <div id="Image_Box">
                     <img id="Home_Image_1" src="../assets/images/labrador-retriever-1210559_960_720.jpg">
-                    <!-- Note: Add image credit: "https://pixabay.com/photos/labrador-retriever-dog-pet-1210559/" -->
+                    <a href="https://pixabay.com/photos/labrador-retriever-dog-pet-1210559/">Click For Image Source</a>
                 </div>
-                <div id="Intro_Text_Box">
+                <div>
                     <!-- Note: Replace filler text during project -->
                     <p>We are <span id="Happy_Valley_Emphasized" onclick="clicker()">Happy Valley Kennels</span>...</p>
-                    <p>And this, is filler text. Filler text is used in situations where a design element, like this paragraph, has been created and positioned, but not assigned a value. This text will eventually be replaced by other text which is relevant to the final design, but for now it is just filler text.</p>
-                    <p>This is also filler text, but in a different paragraph. So if you had two paragraphs worth of text you could place them in different paragraph. You could also place it in one paragraph but that could be difficult to read.</p>
-                </div>
-            </div>
-
-            <div id="row-2">
-                <div>
-                    <p>Interested in our services? Have a question?</p>
-                    <p><a href="/HappyValleyKennels/Sign Up">Make an Account</a> or <a href="../Contact">Reach out to Us</a></p>
+                    <p>A home away from home for your furry friends! Got somewhere you need to be, but can't bring your companion along for the journey? Entrust your beloved companion with our caring staff composed of trained veterinarians and care staff. If you'd like to learn more about us, please consult the <a href="../About/">About page</a>, or <a href="../Contact/">Contact us directly and schedule a visit.</a> We hope to see you soon!</p>
                 </div>
             </div>
         </div>
 
-    </body>
+        <div id="row-2">
+            <div>
+                <p>Interested in our services? Have a question?</p>
+                <p><a href="/HappyValleyKennels/Sign Up">Make an Account</a> or <a href="../Contact">Reach out to Us</a></p>
+            </div>
+        </div>
+    </div>
+
+</body>
+
 </html>
 
 <?php
-    include "../assets/Footer/Footer.php";
+include "../assets/Footer/Footer.php";
 ?>
